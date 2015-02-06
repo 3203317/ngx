88cc2bebccf3b503e32f76ec8e386a27a2324cb3
root <root@nginx-front-1.(none)>
2015-02-06 10:32:20 +0800

    haha

---
+++ front/softserv.lua
@@ -0,0 +1,6 @@
+-- Copyright (C) Xin Huang (3203317@qq.com), http://www.foreworld.net.
+
+ngx.say(ngx.var.tenant_name);
+ngx.say(ngx.var.service_name);
+ngx.say(ngx.var.url_suffix);
+ngx.say(ngx.var.args);
